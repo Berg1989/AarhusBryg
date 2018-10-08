b19c3139ad9bb512099e175898b424f74556b193
Daniel Berg Frederiksen <danielbergfrederiksen1989@gmail.com>
2018-10-08 13:36:35 +0200

    EndOfDat 08-10-2018
    Minor fixes.

--- src/gui/ProduktkategoriWindow.java
+++ src/gui/ProduktkategoriWindow.java
@@ -14,56 +14,56 @@ import storage.Storage;
 
 public class ProduktkategoriWindow extends Stage {
 
-    public ProduktkategoriWindow() {
-        initStyle(StageStyle.UTILITY);
-        initModality(Modality.APPLICATION_MODAL);
-        setResizable(false);
-        setTitle("Administrator Window");
+	public ProduktkategoriWindow() {
+		initStyle(StageStyle.UTILITY);
+		initModality(Modality.APPLICATION_MODAL);
+		setResizable(false);
+		setTitle("Administrator Window");
 
-        GridPane pane = new GridPane();
-        Scene scene = new Scene(pane);
-        initContent(pane);
-        setScene(scene);
-    }
+		GridPane pane = new GridPane();
+		Scene scene = new Scene(pane);
+		initContent(pane);
+		setScene(scene);
+	}
 
-    private ListView<ProduktKategori> lwPKategori;
-    private Button btnOpret, btnRemove, btnLuk, btnGaaTil;
-    private Label lbPKategori;
+	private ListView<ProduktKategori> lwPKategori;
+	private Button btnOpret, btnRemove, btnLuk, btnGaaTil;
+	private Label lbPKategori;
 
-    private void initContent(GridPane pane) {
-        pane.setPadding(new Insets(10));
-        pane.setHgap(10);
-        pane.setVgap(10);
-        pane.setGridLinesVisible(false);
+	private void initContent(GridPane pane) {
+		pane.setPadding(new Insets(10));
+		pane.setHgap(10);
+		pane.setVgap(10);
+		pane.setGridLinesVisible(false);
 
-        lbPKategori = new Label("Produktkategori:");
-        pane.add(lbPKategori, 0, 0);
+		lbPKategori = new Label("Produktkategori:");
+		pane.add(lbPKategori, 0, 0);
 
-        lwPKategori = new ListView();
-        pane.add(lwPKategori, 0, 1, 1, 3);
-        lwPKategori.setPrefHeight(250);
-        lwPKategori.setPrefWidth(180);
-        lwPKategori.getItems().addAll(Storage.getAllProduktKategorier());
+		lwPKategori = new ListView<>();
+		pane.add(lwPKategori, 0, 1, 1, 3);
+		lwPKategori.setPrefHeight(250);
+		lwPKategori.setPrefWidth(180);
+		lwPKategori.getItems().addAll(Storage.getAllProduktKategorier());
 
-        btnOpret = new Button("Opret Kategori");
-        pane.add(btnOpret, 1, 1);
-        btnOpret.setOnAction(event -> btnOpretAction());
+		btnOpret = new Button("Opret Kategori");
+		pane.add(btnOpret, 1, 1);
+		btnOpret.setOnAction(event -> btnOpretAction());
 
-        btnRemove = new Button("Slet Kategori");
-        pane.add(btnRemove, 1, 2);
+		btnRemove = new Button("Slet Kategori");
+		pane.add(btnRemove, 1, 2);
 
-        btnLuk = new Button("Luk");
-        pane.add(btnLuk, 1, 4);
+		btnLuk = new Button("Luk");
+		pane.add(btnLuk, 1, 4);
 
-        btnGaaTil = new Button("Gå Til");
-        pane.add(btnGaaTil, 0, 4);
+		btnGaaTil = new Button("Gå Til");
+		pane.add(btnGaaTil, 0, 4);
 
-    }
+	}
 
-    private void btnOpretAction() {
-        OpretKategoriWindow okw = new OpretKategoriWindow();
-        okw.showAndWait();
+	private void btnOpretAction() {
+		OpretKategoriWindow okw = new OpretKategoriWindow();
+		okw.showAndWait();
 
-    }
+	}
 
 }
--- src/service/Service.java
+++ src/service/Service.java
@@ -2,61 +2,51 @@ package service;
 
 import model.Produkt;
 import model.ProduktKategori;
+import storage.Storage;
 
 public class Service {
-    private static Service service;
-    private Storage storage;
+	private static Service service;
+	private Storage storage;
 
-    private Service() {
-        storage = new Storage();
-    }
+	private Service() {
+		storage = new Storage();
+	}
 
-    public static Service getService() {
-        if (service == null) {
-            service = new Service();
-        }
-        return service;
-    }
+	public static Service getService() {
+		if (service == null) {
+			service = new Service();
+		}
+		return service;
+	}
 
-    public static Service getTestService() {
-        return new Service();
-    }
+	public static Service getTestService() {
+		return new Service();
+	}
 
 	public static Produkt opretProdukt(ProduktKategori pk, String navn, double pris, String str) {
 		Produkt p = pk.createProdukt(navn, pris, str);
 		return p;
 	}
 
-    public Produkt opretProdukt(ProduktKategori pk, String navn, double pris, String str) {
-        Produkt p = new Produkt(navn, pris, str);
-        Storage.addProdukt(pk, p);
-        return p;
-    }
-
-	public void initStorage() {
-		ProduktKategori pk1 = Service.opretProduktKategori("Kategori4");
-		ProduktKategori pk2 = Service.opretProduktKategori("Kategori5");
-		ProduktKategori pk3 = Service.opretProduktKategori("Kategori6");
-		Service.opretProdukt(pk1, "Øl", 35, "0.33 L");
-		Service.opretProdukt(pk1, "Tis", 22, "0.20 CL");
-		Service.opretProdukt(pk2, "Vodka", 120, "1 L");
-		Service.opretProdukt(pk3, "Single Malt Whisky", 350, "0.66 L");
-		Service.opretProdukt(pk3, "Saftevand", 10, "1.5 L");
+	public ProduktKategori opretProduktKategori(String navn) {
+		ProduktKategori pk = new ProduktKategori(navn);
+		Storage.addProduktKategori(pk);
+		return pk;
 	}
 
-    public void initStorage() {
+	public void initStorage() {
 
-        // Produk Kategorier
-        ProduktKategori pk1 = opretProduktKategori("Kategori1");
-        ProduktKategori pk2 = opretProduktKategori("Kategori2");
-        ProduktKategori pk3 = opretProduktKategori("Kategori3");
+		// Produk Kategorier
+		ProduktKategori pk1 = opretProduktKategori("Kategori1");
+		ProduktKategori pk2 = opretProduktKategori("Kategori2");
+		ProduktKategori pk3 = opretProduktKategori("Kategori3");
 
-        // Produkter
-        opretProdukt(pk1, "Øl", 35, "0.33 L");
-        opretProdukt(pk1, "Tis", 22, "0.20 CL");
-        opretProdukt(pk2, "Vodka", 120, "1 L");
-        opretProdukt(pk3, "Single Malt Whisky", 350, "0.66 L");
-        opretProdukt(pk3, "Saftevand", 10, "1.5 L");
-    }
+		// Produkter
+		opretProdukt(pk1, "Øl", 35, "0.33 L");
+		opretProdukt(pk1, "Tis", 22, "0.20 CL");
+		opretProdukt(pk2, "Vodka", 120, "1 L");
+		opretProdukt(pk3, "Single Malt Whisky", 350, "0.66 L");
+		opretProdukt(pk3, "Saftevand", 10, "1.5 L");
+	}
 
 }
