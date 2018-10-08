1afb911e70193941ef2212d55eaee784901d5be2
Daniel Berg Frederiksen <danielbergfrederiksen1989@gmail.com>
2018-10-08 11:31:50 +0200

    initStorage

--- src/service/Service.java
+++ src/service/Service.java
@@ -19,10 +19,28 @@ public class Service {
 	}
 
 	// ProduktKategori
+	public static ProduktKategori opretProduktKategori(String navn) {
+		ProduktKategori pk = new ProduktKategori(navn);
+		return pk;
+	}
+
 	public static Produkt opretProdukt(ProduktKategori pk, String navn, double pris, String str) {
 		Produkt p = new Produkt(navn, pris, str);
 		Storage.addProdukt(pk, p);
 		return p;
+	}
+
+	// -----------------------------------------------------------------------------------------
+
+	public static void initStorage() {
+		ProduktKategori pk1 = Service.opretProduktKategori("Kategori4");
+		ProduktKategori pk2 = Service.opretProduktKategori("Kategori5");
+		ProduktKategori pk3 = Service.opretProduktKategori("Kategori6");
+		Service.opretProdukt(pk1, "Øl", 35, "0.33 L");
+		Service.opretProdukt(pk1, "Tis", 22, "0.20 CL");
+		Service.opretProdukt(pk2, "Vodka", 120, "1 L");
+		Service.opretProdukt(pk3, "Single Malt Whisky", 350, "0.66 L");
+		Service.opretProdukt(pk3, "Saftevand", 10, "1.5 L");
 	}
 
 }
