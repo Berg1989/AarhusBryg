0257fdff08f5231f4e88767b792911ae6d157043
MadsOellgaard <madsoellgaard@hotmail.com>
2018-10-08 11:59:20 +0200

    Gui Næsten færdig
    Fikset Produktkategori med Arraylist

--- src/gui/ProduktkategoriWindow.java
+++ src/gui/ProduktkategoriWindow.java
@@ -2,10 +2,14 @@ package gui;
 
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import model.ProduktKategori;
 
 public class ProduktkategoriWindow extends Stage {
 
@@ -21,11 +25,36 @@ public class ProduktkategoriWindow extends Stage {
         setScene(scene);
     }
 
+    private ListView<ProduktKategori> lwPKategori;
+    private Button btnOpret, btnRemove, btnLuk, btnGaaTil;
+    private Label lbPKategori;
+
     private void initContent(GridPane pane) {
         pane.setPadding(new Insets(10));
         pane.setHgap(10);
         pane.setVgap(10);
         pane.setGridLinesVisible(false);
 
+        lbPKategori = new Label("Produktkategori:");
+        pane.add(lbPKategori, 0, 0);
+
+        lwPKategori = new ListView();
+        pane.add(lwPKategori, 0, 1, 1, 3);
+        lwPKategori.setPrefHeight(250);
+        lwPKategori.setPrefWidth(180);
+
+        btnOpret = new Button("Opret Kategori");
+        pane.add(btnOpret, 1, 1);
+
+        btnRemove = new Button("Slet Kategori");
+        pane.add(btnRemove, 1, 2);
+
+        btnLuk = new Button("Luk");
+        pane.add(btnLuk, 1, 4);
+
+        btnGaaTil = new Button("Gå Til");
+        pane.add(btnGaaTil, 0, 4);
+
     }
+
 }
--- src/service/Service.java
+++ src/service/Service.java
@@ -5,42 +5,51 @@ import model.ProduktKategori;
 import storage.Storage;
 
 public class Service {
-	private static Service service;
+    private static Service service;
+    private Storage storage;
 
-	public static Service getService() {
-		if (service == null) {
-			service = new Service();
-		}
-		return service;
-	}
+    private Service() {
+        storage = new Storage();
+    }
 
-	public static Service getTestService() {
-		return new Service();
-	}
+    public static Service getService() {
+        if (service == null) {
+            service = new Service();
+        }
+        return service;
+    }
 
-	// ProduktKategori
-	public static ProduktKategori opretProduktKategori(String navn) {
-		ProduktKategori pk = new ProduktKategori(navn);
-		return pk;
-	}
+    public static Service getTestService() {
+        return new Service();
+    }
 
-	public static Produkt opretProdukt(ProduktKategori pk, String navn, double pris, String str) {
-		Produkt p = new Produkt(navn, pris, str);
-		Storage.addProdukt(pk, p);
-		return p;
-	}
+    // ProduktKategori
+    public ProduktKategori opretProduktKategori(String navn) {
+        ProduktKategori pk = new ProduktKategori(navn);
+        return pk;
+    }
 
-	// -----------------------------------------------------------------------------------------
+    public Produkt opretProdukt(ProduktKategori pk, String navn, double pris, String str) {
+        Produkt p = new Produkt(navn, pris, str);
+        Storage.addProdukt(pk, p);
+        return p;
+    }
 
-	public static void initStorage() {
-		ProduktKategori pk1 = Service.opretProduktKategori("Kategori4");
-		ProduktKategori pk2 = Service.opretProduktKategori("Kategori5");
-		ProduktKategori pk3 = Service.opretProduktKategori("Kategori6");
-		Service.opretProdukt(pk1, "Øl", 35, "0.33 L");
-		Service.opretProdukt(pk1, "Tis", 22, "0.20 CL");
-		Service.opretProdukt(pk2, "Vodka", 120, "1 L");
-		Service.opretProdukt(pk3, "Single Malt Whisky", 350, "0.66 L");
-		Service.opretProdukt(pk3, "Saftevand", 10, "1.5 L");
-	}
+    // -----------------------------------------------------------------------------------------
+
+    public void initStorage() {
+
+        // Produk Kategorier
+        ProduktKategori pk1 = opretProduktKategori("Kategori4");
+        ProduktKategori pk2 = opretProduktKategori("Kategori5");
+        ProduktKategori pk3 = opretProduktKategori("Kategori6");
+
+        // Produkter
+        opretProdukt(pk1, "Øl", 35, "0.33 L");
+        opretProdukt(pk1, "Tis", 22, "0.20 CL");
+        opretProdukt(pk2, "Vodka", 120, "1 L");
+        opretProdukt(pk3, "Single Malt Whisky", 350, "0.66 L");
+        opretProdukt(pk3, "Saftevand", 10, "1.5 L");
+    }
 
 }
