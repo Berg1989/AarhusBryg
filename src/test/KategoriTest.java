5ab231d015ac2e7ad57cb282bc3a8addd1b18a3e
Madeleine <madeleine.harbom@gmail.com>
2018-10-08 11:43:39 +0200

    BabyTest
    Tiny tiny test

---
+++ src/test/KategoriTest.java
@@ -0,0 +1,29 @@
+package test;
+import model.ProduktKategori;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Produkt;
+
+public class KategoriTest {
+	ProduktKategori k;
+	Produkt p;
+	
+	@Before
+	public void setUp() {
+		k = new ProduktKategori("Oel");
+	}
+	
+	@Test
+	public void testCreateProcukt() {
+		p = k.createProdukt("Pilser",50.0, "50 cL");
+		assertNotNull(p);
+	}
+	
+	
+	
+	
+}
