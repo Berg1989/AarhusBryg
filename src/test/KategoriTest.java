544a109a9ff6ae89834555edb76b133d6e21f019
Daniel Berg Frederiksen <danielbergfrederiksen1989@gmail.com>
2018-10-09 11:48:22 +0200

    klippekort Tests

--- src/test/KategoriTest.java
+++ src/test/KategoriTest.java
@@ -1,5 +1,4 @@
 package test;
-import model.ProduktKategori;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -8,41 +7,42 @@ import org.junit.Before;
 import org.junit.Test;
 
 import model.Produkt;
+import model.ProduktKategori;
 
 public class KategoriTest {
-	ProduktKategori k;
-	Produkt p;
-	
+	private ProduktKategori k;
+	private Produkt p;
+
 	@Before
 	public void setUp() {
 		k = new ProduktKategori("Oel");
 	}
-	
+
 	@Test
 	public void testCreateProdukt1() {
-		p = k.createProdukt("Pilser",50.0, "50 cL");
+		p = k.createProdukt("Pilser", 50.0, "50 cL");
 		assertNotNull(p);
 	}
-	
-	@Test (expected = java.lang.Error.class)
+
+	@Test(expected = java.lang.Error.class)
 	public void testCreateProduct2() {
 		p = k.createProdukt(50.0, 100.0, "50 cL");
 	}
-	
-	@Test (expected = java.lang.Error.class)
+
+	@Test(expected = java.lang.Error.class)
 	public void testCreateProduct3() {
-		p = k.createProdukt("Bock", "Dyr","50 cL");
+		p = k.createProdukt("Bock", "Dyr", "50 cL");
 	}
 
-	@Test (expected = java.lang.Error.class)
+	@Test(expected = java.lang.Error.class)
 	public void testCreateProduct4() {
 		p = k.createProdukt("Pilsner", 50.0, 100.0);
 	}
-	
+
 	@Test
 	public void testGetNavn() {
 		k.toString();
-		assertEquals(k.toString(), "Oel");
+		assertEquals("Oel", k.toString());
 	}
-	
+
 }
