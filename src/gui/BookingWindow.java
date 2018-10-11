edc0abab8781951f182339928bb5db0e96fd683d
MadsOellgaard <madsoellgaard@hotmail.com>
2018-10-11 11:51:28 +0200

    Anlæg Gui

--- src/gui/BookingWindow.java
+++ src/gui/BookingWindow.java
@@ -10,46 +10,49 @@ import javafx.stage.StageStyle;
 
 public class BookingWindow extends Stage {
 
-    public BookingWindow() {
-        initStyle(StageStyle.UTILITY);
-        initModality(Modality.APPLICATION_MODAL);
-        setResizable(false);
-        setTitle("Booking");
+	public BookingWindow() {
+		initStyle(StageStyle.UTILITY);
+		initModality(Modality.APPLICATION_MODAL);
+		setResizable(false);
+		setTitle("Booking");
 
-        GridPane pane = new GridPane();
-        Scene scene = new Scene(pane);
-        initContent(pane);
-        setScene(scene);
-    }
-    // Insert Attributes here:
-    private Button btnRundvisning, btnAnlaeg;
+		GridPane pane = new GridPane();
+		Scene scene = new Scene(pane);
+		initContent(pane);
+		setScene(scene);
+	}
 
-    private void initContent(GridPane pane) {
-        pane.setPadding(new Insets(10));
-        pane.setHgap(10);
-        pane.setVgap(10);
-        pane.setGridLinesVisible(false);
+	// Insert Attributes here:
+	private Button btnRundvisning, btnAnlaeg;
 
-        btnRundvisning = new Button("Rundvisning");
-        pane.add(btnRundvisning, 0, 1);
-        btnRundvisning.setOnAction(event -> btnRundvisningAction());
-        btnRundvisning.setPrefSize(200, 100);
+	private void initContent(GridPane pane) {
+		pane.setPadding(new Insets(10));
+		pane.setHgap(10);
+		pane.setVgap(10);
+		pane.setGridLinesVisible(false);
 
-        btnAnlaeg = new Button("Anlæg");
-        pane.add(btnAnlaeg, 0, 2);
-        btnAnlaeg.setOnAction(event -> btnAnlaegAction());
-        btnAnlaeg.setPrefSize(200, 100);
+		btnRundvisning = new Button("Rundvisning");
+		pane.add(btnRundvisning, 0, 1);
+		btnRundvisning.setOnAction(event -> btnRundvisningAction());
+		btnRundvisning.setPrefSize(200, 100);
 
-    }
-    
-    public void btnRundvisningAction(){
-        RundvisningWindow rw = new RundvisningWindow();
-        rw.showAndWait();
-    	hide();
-    }
-    
-    public void btnAnlaegAction(){
-    	hide();
-    }
+		btnAnlaeg = new Button("Anlaeg");
+		pane.add(btnAnlaeg, 0, 2);
+		btnAnlaeg.setOnAction(event -> btnAnlaegAction());
+		btnAnlaeg.setPrefSize(200, 100);
+
+	}
+
+	public void btnRundvisningAction() {
+		RundvisningWindow rw = new RundvisningWindow();
+		rw.showAndWait();
+		hide();
+	}
+
+	public void btnAnlaegAction() {
+		AnleagWindow aw = new AnleagWindow();
+		aw.showAndWait();
+		hide();
+	}
 
 }
