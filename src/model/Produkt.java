35f0ce56c34033c411be23e20cc5df06afc96482
Daniel Berg Frederiksen <danielbergfrederiksen1989@gmail.com>
2018-10-12 10:34:35 +0200

    ændret produkt toString()

--- src/model/Produkt.java
+++ src/model/Produkt.java
@@ -2,43 +2,43 @@ package model;
 
 public class Produkt {
 
-    private String navn;
-    private double pris;
-    private String str;
+	private String navn;
+	private double pris;
+	private String str;
 
-    public Produkt(String navn, double pris, String str) {
-        this.navn = navn;
-        this.pris = pris;
-        this.str = str;
-    }
+	public Produkt(String navn, double pris, String str) {
+		this.navn = navn;
+		this.pris = pris;
+		this.str = str;
+	}
 
-    public String getNavn() {
-        return navn;
-    }
+	public String getNavn() {
+		return navn;
+	}
 
-    public void setNavn(String navn) {
-        this.navn = navn;
-    }
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
 
-    public double getPris() {
-        return pris;
-    }
+	public double getPris() {
+		return pris;
+	}
 
-    public void setPris(double pris) {
-        this.pris = pris;
-    }
+	public void setPris(double pris) {
+		this.pris = pris;
+	}
 
-    public String getStr() {
-        return str;
-    }
+	public String getStr() {
+		return str;
+	}
 
-    public void setStr(String str) {
-        this.str = str;
-    }
+	public void setStr(String str) {
+		this.str = str;
+	}
 
-    @Override
-    public String toString() {
-        return navn;
-    }
+	@Override
+	public String toString() {
+		return navn + ", " + str + ", " + pris + "kr" + ".";
+	}
 
 }
