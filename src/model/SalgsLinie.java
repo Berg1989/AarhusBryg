c6b27da530f124f7a1dad3608e4ccc64c7da7226
Madeleine <madeleine.harbom@gmail.com>
2018-10-25 08:51:17 +0200

    Salg

--- src/model/SalgsLinie.java
+++ src/model/SalgsLinie.java
@@ -3,10 +3,25 @@ package model;
 public class SalgsLinie {
 	private int antal;
 	private Produkt produkt;
+	private double produktpris;
 	
-	public SalgsLinie(int antal, Produkt produkt) {
+	public SalgsLinie(int antal, Produkt produkt, SalgSted sted) {
 		this.antal = antal;
 		this.produkt = produkt;
+		this.produktpris = produkt.getStedPrisPris(sted);
+	}
+	
+	public double getPris() {
+		double pris = this.antal *  this.produktpris;
+		return pris;
+	}
+	
+	public double getProduktPris() {
+		return this.produktpris;
+	}
+	
+	public String toString() {
+		return "" +this.antal + "x " + this.produkt.getNavn() + " (" + this.produktpris + ") " + getPris();
 	}
 
 }
--- src/test/ServiceTest.java
+++ src/test/ServiceTest.java
@@ -5,9 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sun.xml.internal.ws.wsdl.writer.document.Service;
+
 import model.ProduktKategori;
 
 public class ServiceTest {
+	private Service s = Service.getService();
 	private ProduktKategori pk;
 
 	
@@ -22,6 +25,10 @@ public class ServiceTest {
 	@Test
 	public void test() {
 		assertEquals(2, pk.getProdukter().size());
+	}
+	
+	public void test2() {
+		Service.remove
 	}
 
 }
