bb6d438cc78c946c1c07969f1ea84d2fe76d77f7
Madeleine <madeleine.harbom@gmail.com>
2018-11-02 08:19:52 +0100

    Good morning push

--- src/gui/UdlejningerWindow.java
+++ src/gui/UdlejningerWindow.java
@@ -18,6 +18,7 @@ import model.Fustage;
 import model.Kulsyre;
 import model.Rentable;
 import model.Udlejning;
+import model.UdlejningsLinje;
 import service.Service;
 
 public class UdlejningerWindow extends Stage {
@@ -41,10 +42,11 @@ public class UdlejningerWindow extends Stage {
 	
 	private ListView<Udlejning> lwUdlejning;
 	private Label lblUdlejninger, lblNavn, lblEmail, lblTlf, lblKvittering;
-	private ListView<Rentable> kvittering;
+	private ListView<UdlejningsLinje> lwKvittering;
 	private TextField txfNavn, txfEmail, txfTlf;
 	private CheckBox chbLevering;
 	private Button btnFjern, btnLuk;
+	private Udlejning u;
 	
 	
 	private void initContent(GridPane pane) {
@@ -58,6 +60,7 @@ public class UdlejningerWindow extends Stage {
 		
 		lwUdlejning = new ListView<>();
 		pane.add(lwUdlejning, 0,1, 2, 6);
+		lwUdlejning.setOnMouseClicked(event -> update());
 		
 		btnFjern = new Button("Fjern");
 		pane.add(btnFjern, 0, 8);
@@ -83,7 +86,30 @@ public class UdlejningerWindow extends Stage {
 		chbLevering = new CheckBox("Levering");
 		pane.add(chbLevering, 1, 6);
 		
+		lblKvittering = new Label("Kvittering");
+		pane.add(lblKvittering, 2, 0);
 		
+		lwKvittering = new ListView<>();
+		pane.add(lwKvittering, 2, 1);
+		
+		
+		
+		
+	}
+	
+	public void update() {
+		if (u != null) { //Just to be safe
+			txfTlf.setText(u.getKundeEmail());
+			txfNavn.setText(u.getKundeNavn());
+			txfEmail.setText(u.getKundeEmail());
+			if (u.getLevering()) {
+				chbLevering.setSelected(true);
+			}
+			else {
+				chbLevering.setSelected(false);
+			}
+		}
+		lwKvittering.getItems().addAll(u.getFullOrdre());
 	}
 }
 
